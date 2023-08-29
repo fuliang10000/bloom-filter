ebc3fab2f70ef36d58a95aa99e8fa48770672a3d
fuliang <fuliang19890809@163.com>
2023-08-29 11:55:38 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,57 @@
+# 基于redis实现的布隆过滤器 #
+
+## 安装 ##
+
+    ```
+    composer require fuliang/bloom-filter
+    ```
+
+## 使用 ##
+
+### 第一步 ###
+
+```php
+<?php
+namespace Fuliang\BloomFilter;
+
+use Redis;
+/**
+ * 重复内容过滤器
+ * 该布隆过滤器总位数为2^32位, 判断条数为2^30条. hash函数最优为3个.(能够容忍最多的hash函数个数)
+ * 使用的三个hash函数为
+ * BKDR, SDBM, JSHash
+ *
+ * 注意, 在存储的数据量到2^30条时候, 误判率会急剧增加, 因此需要定时判断过滤器中的位为1的的数量是否超过50%, 超过则需要清空.
+ */
+class FilterRepeatedComments extends BloomFilterRedis
+{
+    /**
+     * 表示判断重复内容的过滤器
+     * @var string
+     */
+    protected string  $bucket = 'rptc';
+
+    protected array $hashFunction = ['BKDRHash', 'SDBMHash', 'JSHash'];
+
+    protected function getRedisClient(): Redis
+    {
+        return new YourRedis; // 假设这里你已经连接好了
+    }
+}
+```
+
+### 第二步 ###
+
+```php
+<?php
+require_once 'vendor\autoload.php';
+use Fuliang\BloomFilter\FilterRepeatedComments;
+
+$bf = new FilterRepeatedComments();
+$bf->add('item1');
+$bf->add('item2');
+$bf->add('item3');
+$bf->exists('item1'); //true
+$bf->exists('item2'); //true
+$bf->exists('item3'); //true
+```
---
+++ src/BloomFilterRedis.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Fuliang\BloomFilter;
+
+abstract class BloomFilterRedis
+{
+    protected string $bucket;
+
+    protected array $hashFunction;
+
+    private BloomFilterHash $Hash;
+
+    private \Redis $Redis;
+
+    /**
+     * @throws \Exception
+     */
+    public function __construct()
+    {
+        if (! $this->bucket) {
+            throw new \Exception('需要定义bucket和hashFunction', 500);
+        }
+        $this->Hash  = new BloomFilterHash();
+        $this->Redis = $this->getRedisClient();
+    }
+
+    /**
+     * 添加到集合中.
+     * @return array|false|\Redis
+     * @throws \RedisException
+     */
+    public function add(string $string)
+    {
+        $pipe = $this->Redis->multi();
+        foreach ($this->hashFunction as $function) {
+            $hash = $this->Hash->{$function}($string);
+            $pipe->setBit($this->bucket, $hash, 1);
+        }
+        return $pipe->exec();
+    }
+
+    /**
+     * 查询是否存在, 存在的一定会存在, 不存在有一定几率会误判.
+     * @throws \RedisException
+     */
+    public function exists(string $string): bool
+    {
+        $pipe = $this->Redis->multi();
+        $len  = strlen($string);
+        foreach ($this->hashFunction as $function) {
+            $hash = $this->Hash->{$function}($string, $len);
+            $pipe = $pipe->getBit($this->bucket, $hash);
+        }
+        $res = $pipe->exec();
+        foreach ($res as $bit) {
+            if ($bit == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    abstract protected function getRedisClient(): \Redis;
+}
